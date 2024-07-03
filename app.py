3d69f548db50450fac98719165df48763fedd8af
victormarchettimed <victormarchetti.medicina@gmail.com>
2024-07-03 17:39:02 -0300

    Initial commit

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
 import pickle
 import numpy as np
+import logging
 
 app = Flask(__name__)
 
-# Carregar o modelo
-model = pickle.load(open('model/best_model.pkl', 'rb'))
+# Configurar logging
+logging.basicConfig(level=logging.DEBUG)
+
+# Carregar o modelo treinado e o threshold
+with open('model/best_model.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+with open('model/optimal_threshold.pkl', 'rb') as f:
+    optimal_threshold = pickle.load(f)
 
 @app.route('/')
 def home():
@@ -13,12 +21,31 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    features = [int(x) for x in request.form.values()]
-    final_features = [np.array(features)]
-    prediction = model.predict(final_features)
-    output = round(prediction[0], 2)
+    # Obter dados do formulário
+    features = []
+    for feat in ['fever', 'myalgia', 'headache', 'exanthem', 'vomiting', 'nausea', 'back_pain', 'conjunctivitis', 'arthritis', 'arthralgia', 'petechia', 'retro_orbital_pain']:
+        features.append(1 if request.form.get(feat) == 'on' else 0)
+    logging.debug(f"Features recebidas: {features}")
     
-    return render_template('index.html', prediction_text=f'Pontuação Calculada: {output}')
+    # Normalizar os dados
+    features_array = np.array(features).reshape(1, -1)
+    
+    # Fazer a predição
+    prediction_prob = model.predict_proba(features_array)[0]
+    chikungunya_prob = prediction_prob[0]
+    dengue_prob = prediction_prob[1]
+    logging.debug(f"Probabilidade de Chikungunya: {chikungunya_prob}")
+    logging.debug(f"Probabilidade de Dengue: {dengue_prob}")
+    
+    prediction = "Chikungunya" if chikungunya_prob >= optimal_threshold else "Dengue"
+    logging.debug(f"Predição: {prediction}")
+    
+    return jsonify({
+        'prediction_text': f"Probability of Dengue: {dengue_prob:.2f}, Probability of Chikungunya: {chikungunya_prob:.2f}. Prediction: {prediction}",
+        'dengue_prob': dengue_prob,
+        'chikungunya_prob': chikungunya_prob,
+        'optimal_threshold': optimal_threshold
+    })
 
 if __name__ == "__main__":
     app.run(debug=True)
